a6f6c400071a15060d2522ddbda80526e87015a0
valuntiny <459201296@qq.com>
2018-10-09 00:09:51 -0400

    solution for plusone

---
+++ simple_testing.py
@@ -0,0 +1,19 @@
+class Solution:
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        num = 0
+        for i in range(len(digits)):
+            num += digits[i] * pow(10, len(digits) - 1 - i)
+
+        num += 1
+
+        return [int(j) for j in str(num)]
+
+plusing = Solution()
+
+a = [1, 2, 3, 4]
+
+print(plusing.plusOne(a))
