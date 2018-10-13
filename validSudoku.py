a1ce7ddb16f38e589f93e25bcabd880ccd85bef0
valuntiny <459201296@qq.com>
2018-10-13 00:17:57 -0400

    turns out, python 3 doesn't have dict.has_key function anymore, we could only use `in` function

--- validSudoku.py
+++ validSudoku.py
@@ -8,30 +8,25 @@ class Solution:
             hash_row = {}
             hash_col = {}
             hash_grid = {}
-            print("i=", i)
 
             for j in range(9): # here, j doesn't represent index for column either
-                print("j=", j)
                 # check the repetition for row
                 if board[i][j] != ".":
-                    if hash_row.has_key(board[i][j]): return False
+                    if (board[i][j] in hash_row): return False
                     else: hash_row.setdefault(board[i][j])
 
                 # check the repetition for column
                 if board[j][i] != ".":
-                    if hash_col.has_key(board[j][i]): return False
+                    if (board[j][i] in hash_col): return False
                     else: hash_col.setdefault(board[j][i])
 
-                start_row = (i // 3) * 3
+                start_row   = (i // 3) * 3
                 start_col = (i % 3) * 3
-                if board[start_row + j % 3][start_col + j % 3] != ".":
-                    if hash_grid.has_key(board[start_row + j // 3][start_col + j % 3]): return False
-                    else: hash_grid.setdefault(board[start_row + j % 3][start_col + j % 3])
+                if board[start_row + j // 3][start_col + j % 3] != ".":
+                    if (board[start_row + j // 3][start_col + j % 3] in hash_grid): return False
+                    else: hash_grid.setdefault(board[start_row + j // 3][start_col + j % 3])
 
         return True
-
-
-
 
 test = Solution()
 
